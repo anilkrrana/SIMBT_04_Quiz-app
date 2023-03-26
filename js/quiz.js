88ccfb2b3f9de6524b2fbf2d638df8d934b4cf53
Anil Rana <102950907+anilkrrana@users.noreply.github.com>
2023-03-26 16:35:33 +0530

    Add files via upload

---
+++ js/quiz.js
@@ -0,0 +1,83 @@
+let quiz = [
+    {
+        question: "What is JavaScript?",
+        option: [
+            "JavaScript is a scripting language used to make the website interactive",
+            "JavaScript is an assembly language used to make the website interactive",
+            "JavaScript is a compiled language used to make the website interactive",
+            "None of the mentioned"
+        ],
+        answer: 1,
+    },
+    {
+        question: "Which of the following is an advantage of the Green sand molding process used in a foundry?",
+        option: [
+            "Appropriate for long production run",
+            "Very low cost process",
+            "Sand is reusable",
+            "All of the mentioned"
+        ],
+        answer: 4,
+    },
+    {
+        question: "The _______ directional properties are imparted to the castings made in foundries?",
+        option: [
+            "Single",
+            "Double",
+            "Spherical",
+            "Multiple"
+        ],
+        answer: 1,
+    },
+    {
+        question: "Which of the following is correct about JavaScript?",
+        option: [
+            "JavaScript is an Object-Based language",
+            "JavaScript is Assembly-language",
+            " JavaScript is an Object-Oriented language",
+            " JavaScript is a High-level language"
+        ],
+        answer: 1,
+    },
+    {
+        question: "Arrays in JavaScript are defined by which of the following statements?",
+        option: [
+            " It is an ordered list of values",
+            "It is an ordered list of objects",
+            "It is an ordered list of string",
+            "It is an ordered list of functions"
+        ],
+        answer: 1,
+    },
+    // {
+    //     question: "Scope of automation in foundry process is ?",
+    //     option: [
+    //         "Low",
+    //         "High",
+    //         "Minimum",
+    //         "Maximum"
+    //     ],
+    //     answer: 1,
+    // },
+    // {
+    //     question: "Scope of automation in foundry process is ?",
+    //     option: [
+    //         "Low",
+    //         "High",
+    //         "Minimum",
+    //         "Maximum"
+    //     ],
+    //     answer: 1,
+    // },
+    // {
+    //     question: "Scope of automation in foundry process is ?",
+    //     option: [
+    //         "Low",
+    //         "High",
+    //         "Minimum",
+    //         "Maximum"
+    //     ],
+    //     answer: 1,
+    // },
+    
+]
